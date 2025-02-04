2b4ffdfeb116cdcf95e3046532c059c0f619ba3e
alexyounkers <ayounker@purdue.edu>
2025-02-04 13:57:27 -0500

    Created swiftUI iOS Xcode project

---
+++ PlotLine/PlotLine/PlotLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlotLineApp.swift
+//  PlotLine
+//
+//  Created by Alex Younkers on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct PlotLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlotLine/PlotLineTests/PlotLineTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlotLineTests.swift
+//  PlotLineTests
+//
+//  Created by Alex Younkers on 2/4/25.
+//
+
+import Testing
+@testable import PlotLine
+
+struct PlotLineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
