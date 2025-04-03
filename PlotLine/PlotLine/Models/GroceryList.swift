4969c6fcfc91ffd19e2ff9342f4a3b4831a6b831
Yash Mehta <imyash@live.com>
2025-04-03 17:09:18 -0400

    frontend updates for ai groc list

--- PlotLine/PlotLine/Models/GroceryList.swift
+++ PlotLine/PlotLine/Models/GroceryList.swift
@@ -10,8 +10,17 @@ import Foundation
 struct GroceryList: Identifiable, Codable {
     var id: UUID
     var name: String
-    var items: [GroceryItem] // Can be replaced with a more detailed model if needed
-    var username: String // Include username to associate the list with the user
+    var items: [GroceryItem]
+    var username: String
+    var isAI: Bool?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case items
+        case username
+        case isAI = "ai"
+    }
 }
 
 struct GroceryItem: Identifiable, Codable, Equatable {
